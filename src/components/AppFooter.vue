e08039d3a3f82bdfc57bfafde91ed6e7aff5a033
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-09-09 19:17:13 +0200

    add footer left list

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -29,6 +29,114 @@ export default {
           url: "#",
         },
       ],
+      linksDc: [
+        {
+          text: "Characters",
+          url: "#",
+        },
+        {
+          text: "Comics",
+          url: "#",
+        },
+        {
+          text: "Movies",
+          url: "#",
+        },
+        {
+          text: "TV",
+          url: "#",
+        },
+        {
+          text: "Games",
+          url: "#",
+        },
+        {
+          text: "Videos",
+          url: "#",
+        },
+        {
+          text: "News",
+          url: "#",
+        },
+      ],
+      shopsDc: [
+        {
+          text: "Shop DC",
+          url: "#",
+        },
+        {
+          text: "Shop DC collectibles",
+          url: "#",
+        },
+      ],
+      dc: [
+        {
+          text: "Terms Of Use",
+          url: "#",
+        },
+        {
+          text: "Privacy policy (New)",
+          url: "#",
+        },
+        {
+          text: "Ad Choices",
+          url: "#",
+        },
+        {
+          text: "Advertising",
+          url: "#",
+        },
+        {
+          text: "Jobs",
+          url: "#",
+        },
+        {
+          text: "Subscriptions",
+          url: "#",
+        },
+        {
+          text: "Talent Workshops",
+          url: "#",
+        },
+        {
+          text: "CPSC Certificates",
+          url: "#",
+        },
+        {
+          text: "Ratings",
+          url: "#",
+        },
+        {
+          text: "Shop Help",
+          url: "#",
+        },
+        {
+          text: "Contact Us",
+          url: "#",
+        },
+      ],
+      sites: [
+        {
+          text: "DC",
+          url: "#",
+        },
+        {
+          text: "MAD Magazine",
+          url: "#",
+        },
+        {
+          text: "DC Kids",
+          url: "#",
+        },
+        {
+          text: "DC Universe",
+          url: "#",
+        },
+        {
+          text: "DC Power Visa",
+          url: "#",
+        },
+      ],
     };
   },
 };
@@ -47,11 +155,47 @@ export default {
       </ul>
     </div>
   </div>
+  <div class="container_center_footer">
+    <div class="container_center">
+      <div class="container_left">
+        <div>
+          <h3>DC COMICS</h3>
+          <ul>
+            <li v-for="linkDc in linksDc">
+              <a :href="linkDc.url">{{ linkDc.text }} </a>
+            </li>
+          </ul>
+          <h3>SHOP</h3>
+          <ul>
+            <li v-for="shop in shopsDc">
+              <a :href="shop.url">{{ shop.text }} </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3>DC</h3>
+          <ul>
+            <li v-for="Element in dc">
+              <a :href="Element.url">{{ Element.text }} </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3>Sites</h3>
+          <ul>
+            <li v-for="site in sites">
+              <a :href="site.url">{{ site.text }} </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .container_top_footer {
-  height: 200px;
+  padding: 50px 0;
   background-color: #0c7cec;
 }
 
@@ -63,7 +207,7 @@ export default {
   align-items: center;
 }
 
-ul {
+.container_top ul {
   display: flex;
   align-items: center;
   gap: 3rem;
@@ -84,10 +228,40 @@ li:last-child img {
   width: 65px;
 }
 
-a {
+.container_top a {
   display: flex;
   align-items: center;
   text-decoration: none;
   color: white;
+}
+
+.container_center_footer {
+  background-image: url("/footer-bg.jpg");
+}
+
+.container_center {
+  padding-bottom: 40px;
+  margin: 0 15%;
+  display: flex;
+}
+
+h3 {
+  color: white;
+  padding-top: 40px;
+  margin-bottom: 10px;
+}
+.container_center li {
+  margin-bottom: 8px;
+  font-size: 0.8rem;
+  list-style: none;
+}
+.container_center a {
+  text-decoration: none;
+  color: gray;
+}
+
+.container_left {
+  display: flex;
+  gap: 4rem;
 }
 </style>
